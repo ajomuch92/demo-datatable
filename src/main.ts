451f0aab0bf05e69ba2cf421491bb34a25846b0a
Aarón J. Montes <aaronmontes_92@yahoo.es>
2023-04-01 16:45:00 -0600

    feat: 🎸 Removing vue tippy dependency

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
 import { createApp } from 'vue'
 import './style.css';
 import App from './App.vue';
-import VueTippy from 'vue-tippy';
-import 'tippy.js/dist/tippy.css';
 
 createApp(App)
-  .use(VueTippy)
   .mount('#app');
 
