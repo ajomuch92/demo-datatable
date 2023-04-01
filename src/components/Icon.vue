5f6709919b9b42e474cc8504e14c1c5c7ff55c2a
Aarón J. Montes <aaronmontes_92@yahoo.es>
2023-04-01 13:09:44 -0600

    feat: 🎸 Adding icon component to project

---
+++ src/components/Icon.vue
@@ -0,0 +1,17 @@
+<template>
+  <i class="bi" :class="`bi-${name}`"></i>
+</template>
+
+<script setup lang="ts">
+  defineProps({
+    name: {
+      type: String,
+      required: true,
+      validator: (val: string) => !val.includes('bi')
+    }
+  })
+</script>
+
+<style>
+
+</style>
